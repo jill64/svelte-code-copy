2b7c2573ff3c67d2ec21975b6e59bb4358a928bb
jill64 <intents.turrets0h@icloud.com>
2023-12-11 14:06:46 +0900

    add page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { CodeCopy } from '$lib'
+  import { theme, toast } from '@jill64/npm-demo-layout'
+  import { HighlightSvelte } from '@jill64/npm-demo-layout/highlight'
+  import { code } from './code'
+</script>
+
+<main>
+  <output>
+    <CodeCopy
+      color="inherit"
+      onCopy={(promise) =>
+        $toast.promise(promise, {
+          success: 'Copied!',
+          error: 'Failed to copy',
+          loading: 'Copying...'
+        })}
+      effect={$theme === 'dark' ? 'pop' : 'push'}
+    >
+      <HighlightSvelte code={code()}></HighlightSvelte>
+    </CodeCopy>
+  </output>
+</main>
